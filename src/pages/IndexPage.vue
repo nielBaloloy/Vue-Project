0f5ed2d437481e0a493dde20a30c08317f64d325
nielBaloloy <baloloycrisniel@gmail.com>
2024-04-14 19:08:49 +0800

    FirstPush

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="flex flex-center">
+ 
+    user
+    <q-input outlined v-model="form.user"  />
+    pass
+    <q-input outlined v-model="form.pass" />
+    <q-btn color="white" text-color="black" label="Submit"  @click="sendlogs(form)"/>
+    {{ logs }}
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import { api } from "../boot/axios";
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  setup(){
+    let form =ref({
+      user:null,
+      pass:null,
+    });
+    let logs=ref();
+  const sendlogs = (payload) => {
+ 
+  return new Promise((resolve, reject) => {
+    api
+      .post("api.php", { info: payload })
+      .then((response) => {
+         logs.value = response.data.Result;
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+    return{
+      form,
+      sendlogs,
+      logs
+
+    }
+  }
+})
+</script>
